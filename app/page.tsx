5787b3cbc657b84cf7de94887ab6589c05192197
Gregory Kolb <gkolb100@gmail.com>
2025-05-07 19:02:03 -0600

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Trade Save!</h1>
-      <p>
-        A site to save all trade queries for POE 2 trade
-      </p>
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold mb-6">Welcome to Trade Save</h1>
+      <Button>
+        Add Trade
+      </Button>
+    </main>
   );
 }
